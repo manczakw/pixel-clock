516e4c34b7854985de5574d3a073fc18f9cb8ad1
Wojciech Mańczak <wojciechmanczakpoczta@gmail.com>
2022-10-20 21:51:20 +0200

    Added KiCad schematic Added new test to wstest Added 2x8 0.2mm case test

--- Software/wstest.py
+++ Software/wstest.py
@@ -4,6 +4,7 @@ import board
 import neopixel
 import time
 import random
+import sys
 
 NumOfLeds = 256
 NumOfRows = 8
@@ -13,7 +14,7 @@ DataPin = board.D18
 
 pixels = neopixel.NeoPixel(DataPin, NumOfLeds)
 
-while True:
+def OneHot():
     for i in range(NumOfLeds):
            R = random.randint(0, 255)
            G = random.randint(0, 255-R)
@@ -21,6 +22,34 @@ while True:
            pixels[i] = (R,G,B)
            time.sleep(0.010)
            pixels[i] = (0,0,0) 
+
+def TwoRows():
+    for i in range(16):
+        R = random.randint(0, 255)
+        G = random.randint(0, 255-R)
+        B = random.randint(0, 255-G-R)
+        pixels[i] = (R,G,B)
+    time.sleep(5)
+    for i in range(16):
+        pixels[i] = (0,0,0)
+    time.sleep(5)
+
+def BlackDown():
+    for i in range(NumOfLeds):  
+        pixels[i] = (0,0,0) 
+
+while True:
+    OneHot()
+
+
+
+
+
+
+
+
+            
+          
        
     
    
